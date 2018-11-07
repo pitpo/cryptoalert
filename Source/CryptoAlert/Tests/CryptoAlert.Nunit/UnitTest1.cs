c6d9571c940442db254c2254a9fe9d4a7741c6a9
Szymon Smykała <ctk7000@gmail.com>
2018-11-07 18:34:51 +0100

    Added Nunit project

---
+++ Source/CryptoAlert/Tests/CryptoAlert.Nunit/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+
+
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
