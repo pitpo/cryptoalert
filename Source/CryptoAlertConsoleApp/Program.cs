bacb1b176823494f88a37b20cb833e6f8d30fd37
Szymon Smykała <ctk7000@gmail.com>
2018-12-16 20:51:48 +0100

    removed useless method

--- Source/CryptoAlertConsoleApp/Program.cs
+++ Source/CryptoAlertConsoleApp/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Configuration;
 using System.Threading.Tasks;
-using CryptoAlertCore.CoinsInformation.DTO.Coins;
 using CryptoAlertCore.CoinsInformation.Factories;
-using CryptoAlertCore.DBRepository;
-using CryptoAlertCore.Models;
-using LiteDB;
 
 namespace CryptoAlertConsoleApp
 {
@@ -23,33 +18,9 @@ namespace CryptoAlertConsoleApp
             Console.WriteLine(result);
         }
 
-        public static void TestDB()
-        {
-            IDBRepository<User> userRepository = new DBRepository<User>(ConfigurationManager.ConnectionStrings["LiteDB"].ConnectionString);
-            User u1 = new User();
-            u1.Email = "twojastara@dupajasia.com";
-            User u2 = new User();
-            u2.Email = "jakubkiermasz@pear.com";
-            Console.WriteLine(userRepository.Insert(u1));
-            Console.WriteLine(userRepository.Insert(u2));
-
-            User r1 = userRepository.GetByKey<String>("Email", "jakubkiermasz@pear.com");
-
-            r1.Name = "Dzban";
-            r1.HashedPassword = "12345";
-
-            Console.WriteLine(userRepository.Update(r1));
-
-            User r2 = userRepository.GetByKey<String>("Email", "jakubkiermasz@pear.com");
-
-            User r3 = userRepository.GetById(1);
-            Console.Write("");
-        }
-
         static void Main(string[] args)
         {
             //Start().Wait();
-            TestDB();
             Console.Read();
         }
     }
