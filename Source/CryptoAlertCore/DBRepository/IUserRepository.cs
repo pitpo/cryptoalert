f069db3e042b14a27bd7125b71c371dc12cac75f
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-16 19:56:09 +0100

    CA-008 Cleanup imports

--- Source/CryptoAlertCore/DBRepository/IUserRepository.cs
+++ Source/CryptoAlertCore/DBRepository/IUserRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CryptoAlertCore.Models;
-using LiteDB;
+﻿using CryptoAlertCore.Models;
 
 namespace CryptoAlertCore.DBRepository
 {
--- Source/CryptoAlertCore/DBRepository/UserRepository.cs
+++ Source/CryptoAlertCore/DBRepository/UserRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CryptoAlertCore.Models;
+﻿using CryptoAlertCore.Models;
 using LiteDB;
 
 namespace CryptoAlertCore.DBRepository
