fb393de24223eb68dcf3eb6212fdcaf6f8af866b
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-16 21:55:38 +0100

    CA-006 Rename sample email field

--- Source/Tests/CryptoAlert.Nunit/Authentication/UserAuthenticatorTests.cs
+++ Source/Tests/CryptoAlert.Nunit/Authentication/UserAuthenticatorTests.cs
@@ -14,7 +14,7 @@ namespace CryptoAlert.NUnit.Authentication
     class UserAuthenticatorTests
     {
         private User _user;
-        private const String _sample_email = "jankowalski@uj.edu.pl";
+        private const String _SAMPLE_EMAIL = "jankowalski@uj.edu.pl";
         private Mock<IDBRepository<User>> _userDbRepositoryMock;
         private Mock<IBCryptWrapper> _bCryptWrapperMock;
 
@@ -27,15 +27,15 @@ namespace CryptoAlert.NUnit.Authentication
             _userDbRepositoryMock = new Mock<IDBRepository<User>>();
             _bCryptWrapperMock = new Mock<IBCryptWrapper>();
 
-            _userDbRepositoryMock.Setup(x => x.GetByKey<String>("Email", _sample_email)).Returns(_user);
+            _userDbRepositoryMock.Setup(x => x.GetByKey<String>("Email", _SAMPLE_EMAIL)).Returns(_user);
             _bCryptWrapperMock.Setup(x => x.Verify(It.IsAny<string>(), It.IsAny<string>())).Returns(false);
             _bCryptWrapperMock.Setup(x => x.Verify("correctpass", _user.HashedPassword)).Returns(true);
 
             _sut = new UserAuthenticator(_userDbRepositoryMock.Object, _bCryptWrapperMock.Object);
         }
 
-        [TestCase(_sample_email, "correctpass", ExpectedResult = true)]
-        [TestCase(_sample_email, "incorrectpass", ExpectedResult = false)]
+        [TestCase(_SAMPLE_EMAIL, "correctpass", ExpectedResult = true)]
+        [TestCase(_SAMPLE_EMAIL, "incorrectpass", ExpectedResult = false)]
         public bool TestPasswordVerification(string email, string unhashedPassword)
         {
             // Act
@@ -49,7 +49,7 @@ namespace CryptoAlert.NUnit.Authentication
         {
             User user = new User()
             {
-                Email = _sample_email,
+                Email = _SAMPLE_EMAIL,
                 HashedPassword = "hashedpass",
                 Id = 1,
                 Name = "Jan Kowalski"
