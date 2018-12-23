a9dfb90085900bc2310b9236d94dc17b9d286d33
Szymon Smykała <ctk7000@gmail.com>
2018-12-23 16:43:19 +0100

    CA-014 added Controller and ViewModel

---
+++ Source/CryptoAlert/Controllers/CoinController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryptoAlert.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CryptoAlert.WebApp.Controllers
+{
+    public class CoinController : Controller
+    {
+        private CoinViewModel _coinViewModel;
+        public CoinController()
+        {
+            _coinViewModel = new CoinViewModel();
+        }
+
+        [Route("/Coin/{coinId}")]
+        public IActionResult Index(int coinId)
+        {
+
+            return View(_coinViewModel);
+        }
+    }
+}
---
+++ Source/CryptoAlert/Models/CoinViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CryptoAlertCore.CoinsInformation.DTO.Coins;
+using CryptoAlertCore.CoinsInformation.Factories;
+using CryptoAlertCore.CoinsInformation.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CryptoAlert.WebApp.Models
+{
+    public class CoinViewModel : PageModel
+    {
+
+        private readonly ICoinsInformationService _coinsInformationService;
+
+        public CoinViewModel()
+        {
+            _coinsInformationService = new CoinInformationServiceFactory().Create();
+        }
+
+        public Coin GetCoinInformation(int coinId)
+        {
+            return _coinsInformationService.GetCoinAsync(coinId).Result;
+        }
+
+        public Coin Coin;
+
+    }
+}
