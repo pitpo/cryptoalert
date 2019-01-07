580d923ba177a96189c3fe5eacc65c35d54dc69c
Piotr Gabrielski <ppitpo@gmail.com>
2019-01-07 21:48:13 +0100

    CA-009 Add user favorite coins frontend

---
+++ Source/CryptoAlert/Controllers/FavoriteCoinsController.cs
@@ -0,0 +1,17 @@
+﻿using CryptoAlert.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoAlert.WebApp.Controllers
+{
+    public class FavoriteCoinsController : Controller
+    {
+        private readonly FavoriteCoinsViewModel _favoriteCoinsViewModel = new FavoriteCoinsViewModel();
+
+        public IActionResult Index()
+        {
+            ViewData.Add("Coins", _favoriteCoinsViewModel.ListOfAllFavoriteCoins);
+
+            return View(_favoriteCoinsViewModel);
+        }
+    }
+}
---
+++ Source/CryptoAlertCore/UserFavorites/Factories/UserFavoritesCoinsServiceFactory.cs
@@ -0,0 +1,16 @@
+﻿using CryptoAlertCore.Configuration;
+using CryptoAlertCore.UserFavorites.Repositories;
+using CryptoAlertCore.UserFavorites.Services;
+
+namespace CryptoAlertCore.UserFavorites.Factories
+{
+	public class UserFavoritesCoinsServiceFactory : IUserFavoritesCoinsServiceFactory
+	{
+		public IUserFavoritesCoinsService Create()
+		{
+			ICryptoAlertConfiguration cryptoAlertConfiguration = new CryptoAlertConfiguration();
+			IUserFavoritesCoinsRepository userFavoritesCoinsRepository = new UserFavoritesCoinsRepository(cryptoAlertConfiguration.UserFavoritesCoinsDatabaseConnectionString);
+			return new UserFavoritesCoinsService(userFavoritesCoinsRepository);
+		}
+	}
+}
