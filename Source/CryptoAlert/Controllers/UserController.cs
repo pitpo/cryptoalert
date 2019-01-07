950823f7c4129d6ec99dea405e83b7c08fe30c77
Szymon Smykała <ctk7000@gmail.com>
2019-01-07 21:08:25 +0100

    CA-021-refactored-code

--- Source/CryptoAlert/Controllers/UserController.cs
+++ Source/CryptoAlert/Controllers/UserController.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CryptoAlert.WebApp.Controllers
 {
-    public class UserController : Controller
-    {
-	    private readonly UserViewModel _userViewModel = new UserViewModel();
+	public class UserController : Controller
+	{
+		private readonly UserViewModel _userViewModel = new UserViewModel();
 
-        public IActionResult Index()
-        {
-            return View(_userViewModel);
-        }
-    }
+		public IActionResult Index()
+		{
+			return View(_userViewModel);
+		}
+	}
 }
