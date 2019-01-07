baccf82fd70a3f5d7c1dac40e2764e9afda2356d
Szymon Smykała <ctk7000@gmail.com>
2019-01-07 21:01:05 +0100

    CA-021-added-displaying-user-profile

---
+++ Source/CryptoAlert/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using CryptoAlert.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoAlert.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+	    private readonly UserViewModel _userViewModel = new UserViewModel();
+
+        public IActionResult Index()
+        {
+            return View(_userViewModel);
+        }
+    }
+}
